88fb68e51a0ac8a27275b624f5fe1628a67af3ff
Will <montero_w@live.com>
2022-02-02 16:59:37 -0600

    Made made insert and the other classes
    Working on removing nodes from the Tree

--- CDDS_BinaryTree/BinaryTree.h
+++ CDDS_BinaryTree/BinaryTree.h
@@ -45,9 +45,106 @@ private:
 	/// <returns>Whether or not a node matching the value could be found</returns>
 	bool findNode(T searchValue, TreeNode<T>*& nodeFound, TreeNode<T>*& nodeParent);
 
-	void draw(TreeNode<T>*, int x, int y, int horizontalSpacing, TreeNode<T>* selected = nullptr);
+	void draw(TreeNode<T>* currentNode, int x, int y, int horizontalSpacing, TreeNode<T>* selected = nullptr);
 
 	TreeNode<T>* m_root = nullptr;
 };
 
 #endif
+
+template<typename T>
+inline void BinaryTree<T>::draw(TreeNode<T>* currentNode, int x, int y, int horizontalSpacing, TreeNode<T>* selected)
+{
+	//Decrease the horixontal space as the nodes draw
+	horizontalSpacing /= 2;
+
+	//Check if the current node is null
+	if (currentNode)
+	{
+		//Draw the left thr child if this node has one
+		if (currentNode->hasLeft())
+		{
+			//Draw a line between the left child and the current node 
+			DrawLine(x, y, x - horizontalSpacing, y + 80, RED);
+			//Draws the left child
+			draw(currentNode->getLeft(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
+		}
+
+		//Draws the right child if this node has one
+		if (currentNode->hasRight())
+		{
+			//Draw a line between the left child and the current node 
+			DrawLine(x, y, x - horizontalSpacing, y + 80, RED);
+			//Draws the right child
+			draw(currentNode->getRight(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
+		}
+
+		//Draws the current the node 
+		currentNode->draw(x, y, (selected == currentNode));
+	}
+}
+
+template<typename T>
+inline bool BinaryTree<T>::isEmpty() const
+{
+	//If there is no node at the root 
+	if (m_root == nullptr)
+		//. . . retiurns true
+		return true;
+	//if there are only node on the roots but none on the left or right 
+	if (m_root->getLeft() == nullptr|| m_root->getRight() == nullptr)
+		//. . . returns true
+		return true;
+	// other wise 
+	return false;
+}
+
+template<typename T>
+inline void BinaryTree<T>::insert(T value)
+{
+	//Creats a new node that is going to be inserted 
+	TreeNode<T> newNode = new TreeNode<T>(value);
+	
+	//Creats a new current node from the root of the tree 
+	TreeNode<T> currentNode = m_root;
+	
+	//Checks if there us a node in the root first
+	//If there is nothing in the current node 
+	if (currentNode == nullptr)
+	{
+		//Set the current node to be the node 
+		m_root = newNode;
+		//then is breaks out if the function 
+		return;
+	}
+
+	else
+	{
+		//Whiel the current node does not equal nullptr
+		while (currentNode != nullptr)
+		{
+			//if the new node data is greater then the current node data 
+			if (newNode->getData() > currentNode->getData())
+				//Make that current node to be that current nodes right node
+				currentNode = currentNode.getRight();
+			//. . . other wise 
+			else
+				//Make that current node to be that current nodes left node
+				currentNode = currentNode.getLeft();
+		}
+		//Set that current node location to be the new node 
+		currentNode = newNode;
+	}
+
+}
+
+template<typename T>
+inline void BinaryTree<T>::remove(T value)
+{
+	TreeNode<T> currentNode = m_root;
+
+	while (currentNode.getData() != value)
+	{
+
+	}
+}
--- CDDS_BinaryTree/TreeNode.h
+++ CDDS_BinaryTree/TreeNode.h
@@ -24,30 +24,30 @@ public:
 	/// <summary>
 	/// Gets the child to the left of this node
 	/// </summary>
-	TreeNode<T>* getLeft();
+	TreeNode<T>* getLeft() { return m_left; };
 
 	/// <summary>
 	/// Gets the child to the right of this node
 	/// </summary>
-	TreeNode<T>* getRight();
+	TreeNode<T>* getRight() { return m_right; };
 
 	/// <summary>
 	/// Sets the value of the data this node is storing to be the given value
 	/// </summary>
 	/// <param name="value">The value to change the data to</param>
-	void setData(T value);
+	void setData(T value) { m_value = value; };
 
 	/// <summary>
 	/// Sets the left child of this node to be the given node
 	/// </summary>
 	/// <param name="node">The node to set as this nodes new child</param>
-	void setLeft(TreeNode<T>* node);
+	void setLeft(TreeNode<T>* node) { m_left = node; };
 
 	/// <summary>
 	/// Sets the left child of this node to be the given node
 	/// </summary>
 	/// <param name="node">The node to set as this nodes new child</param>
-	void setRight(TreeNode<T>* node);
+	void setRight(TreeNode<T>* node) { m_right = node; };
 
 	void draw(int x, int y, bool selected = false);
 
@@ -58,5 +58,44 @@ private:
 	TreeNode<T>* m_right;
 };
 
+template<typename T>
+inline void TreeNode<T>::draw(int x, int y, bool selected)
+{
+	//Creats a array to store the string repesentation of thw value
+	static char buffer[10];
 
+	//Converts the value to a string and store it in the array
+	sprintf(buffer, "%d", m_value);
 
+	//Draw the circle to represent the node
+	DrawCircle(x, y, 30, YELLOW);
+
+	//If the node is the current slected node change its color 
+	if (selected)
+		DrawCircle(x, y, 28, GREEN);
+	else
+		DrawCircle(x, y, 28, BLACK);
+
+	//Draw the value of the node inside it's circle 
+	DrawText(buffer, x - 12, y - 12, 12, WHITE);
+}
+
+template<typename T>
+inline bool TreeNode<T>::hasLeft()
+{
+	//if there is node in the left node 
+	if (m_left != nullptr)
+		return true;
+	//. . . other wise 
+	return false;
+}
+
+template<typename T>
+inline bool TreeNode<T>::hasRight()
+{
+	//if there is node in the right node 
+	if(m_right != nullptr)
+		return true;
+	//. . . other wise 
+	return false;
+}
